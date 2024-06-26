8a229b3e9556df3b1bcb184bc1cdc6ca849fbbd8
Iarley-01 <yarleyalves2016@gmail.com>
2024-06-26 14:15:36 -0300

    criando página de favoritos

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Inicio from "pages/Inicio";
+import Favoritos from "pages/Favoritos";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path="/favoritos" element={<Favoritos />} />
       </Routes>
     </BrowserRouter>
   );
