ebefa4c0b3697be3a5a8591b348c55be23b02c56
Iarley-01 <yarleyalves2016@gmail.com>
2024-06-26 14:01:28 -0300

    renderizando cards dinamicamente

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,8 +1,12 @@
+import styles from "./Inicio.module.css";
+
 import Cabecalho from "components/Cabecalho";
 import Rodape from "components/Rodape";
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
 import Card from "components/Card";
+
+import videos from "json/db.json";
 
 function Inicio() {
   return (
@@ -12,7 +16,9 @@ function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
-      <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format-src&type=png' />
+      <section className={styles.container}>
+        {videos.map((video) => <Card {...video} key={video.id} />)}
+      </section>
       <Rodape />
     </>
   );
