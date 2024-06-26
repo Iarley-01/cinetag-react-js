29f2dd9ec74c10de36c287dafd1230e9dd879c62
Iarley-01 <yarleyalves2016@gmail.com>
2024-06-26 14:28:58 -0300

    criando componente de espaçamento

--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,18 @@ import Favoritos from "pages/Favoritos";
 
 import Cabecalho from "components/Cabecalho";
 import Rodape from "components/Rodape";
+import Container from "components/Container";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Cabecalho />
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/favoritos" element={<Favoritos />} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/favoritos" element={<Favoritos />} />
+        </Routes>
+      </Container>
       <Rodape />
     </BrowserRouter>
   );
