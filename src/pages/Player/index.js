11bd50f639a9496c987bab1b3b169394b49614fa
Iarley-01 <yarleyalves2016@gmail.com>
2024-06-27 12:41:54 -0300

    criando funcionalidade de mostrar o vídeo pelo id da url

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -1,9 +1,17 @@
+import { useParams } from "react-router-dom";
+
 import styles from "./Player.module.css";
 
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
 
+import videos from "json/db.json";
+
 function Player() {
+  const parametros = useParams();
+  const video = videos.find((video) => {
+    return video.id === Number(parametros.id)
+  });
   return (
     <>
       <Banner imagem="player" />
@@ -11,7 +19,13 @@ function Player() {
         <h1>Player</h1>
       </Titulo>
       <section className={styles.container}>
-        
+        <iframe
+        width="560"
+        height="315"
+        src={video.link}
+        title={video.titulo}
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </section>
     </>
   );
